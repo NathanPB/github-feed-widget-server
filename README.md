d92efe092b770615ec58e3c538e659b7f7965629
NathanPB <nathan.pbombana@gmail.com>
2019-10-05 23:25:55 -0300

    Makes it compatible with every oAuth2 server

--- README.md
+++ README.md
@@ -8,8 +8,28 @@ GitHub oAuth2 flow does not supports [Implicit Flow](https://tools.ietf.org/html
 What this project do is redirecting the requests to ``ìndex.php`` to GitHub, including the Client Secret without exposing it to the requester.
 
 # Installation
-1. Create a ``.client_secret`` file and put in it your client secret **AND JUST THE CLIENT SECRET**. Pay attention to spaces or line breaks, they aren't allowed.
+1. Create a ``.env`` file and fill with [the settings](#Settings)
 2. Serve ``index.php`` file somewhere in the internet
 
+# Settings
+
+Each parameters must go in one line, in the format key=value, with no trailing between the key, the ``=`` and the value.
+The parameter order is not important.
+
+| Parameter               | Description                                             | Required | Default     |
+|-------------------------|---------------------------------------------------------|----------|-------------|
+| ``URL``                 | The URL to redirect                                     | True     |             |
+| ``METHOD``              | The request method. Supports only ``GET`` and ``POST``  | False    | ``POST``    |
+| ``CLIENT_SECRET``       | The client secret   | True     |             |
+| ``CLIENT_SECRET_PARAM`` | The name of the param to send the client secret | False | ``client_secret`` |
+| ``REQUIRED_PARAMS``     | The required parameters of the request, separated by ``, `` (comma and space) | False | |
+
+E.g:
+```
+CLIENT_SECRET=d0g315g0d
+URL=https://github.com/login/oauth/access_token
+REQUIRED_PARAMS=code, client_id
+```
+
 # Usage
-Just make the requests to the ``index.php`` file instead of the GitHub server.
+Just make the requests to the ``index.php`` file instead of the oAuth2 server.
--- index.php
+++ index.php
@@ -9,22 +9,58 @@
     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
     */
 
-    function get_secret(){
-        $file = fopen('.client_secret', 'r');
-        $secret = fread($file, filesize('.client_secret'));
+    function get_env(){
+        $file = fopen('.env', 'r');
+        $secret = fread($file, filesize('.env'));
         fclose($file);
-        return $secret;
+
+        $env_array = [];
+        foreach (explode("\n", $secret) as $line) {
+            $kv = explode('=', $line);
+            $env_array[$kv[0]] = $kv[1];
+        }
+
+        return $env_array;
     }
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['code'] && $_POST['client_id']) {
-        echo file_get_contents(
-            "https://github.com/login/oauth/access_token",
-            false,
-            stream_context_create([
-                'http' => [
-                    'method' => 'POST',
-                    'header'  => "Content-type: application/x-www-form-urlencoded",
-                    'content' => http_build_query(array_merge($_POST, ['client_secret' => get_secret()]))
-                ]
-        ]));
+    $env = get_env();
+    $url = $env['URL'];
+    $client_secret = $env['CLIENT_SECRET'];
+    $method = empty($env['METHOD']) ? 'POST' : strtoupper($env['METHOD']);
+    $required_params = empty($env['REQUIRED_PARAMS']) ? [] : explode(', ', $env['REQUIRED_PARAMS']);
+    $client_secret_param = empty($env['CLIENT_SECRET_PARAM']) ? 'client_secret' : $env['CLIENT_SECRET_PARAM'];
+
+    if(!empty($client_secret) || !empty($url)) {
+        if($_SERVER['REQUEST_METHOD'] === $method) {
+            $containedParams = array_intersect($required_params, array_keys($method === 'POST' ? $_POST : $_GET));
+            if(count($containedParams) === count($required_params)) {
+                if($method === 'GET') {
+                    $url .= "?$client_secret_param=$client_secret";
+                    foreach($_GET as $k=>$v) {
+                        $url .= "&$k=>$v";
+                    }
+                    echo file_get_contents($url);
+                } else {
+                    echo file_get_contents(
+                        $url,
+                        false,
+                        stream_context_create([
+                            'http' => [
+                                'method' => $method,
+                                'header'  => "Content-type: application/x-www-form-urlencoded",
+                                'content' => http_build_query(array_merge($_POST, [$client_secret_param => $client_secret]))
+                            ]
+                        ]));
+                }
+            } else {
+                header("HTTP/1.0 422 Unprocessable Entity");
+                die();
+            }
+        } else {
+            header("HTTP/1.0 405 Method Not Allowed");
+            die();
+        }
+    } else {
+        header("HTTP/1.0 500 Internal Server Error");
+        die();
     }
